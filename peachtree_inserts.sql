e95c3f28b8f40394c0ef09cd11105a24fc5aa0b5
uzr6bq <110583756+uzr6bq@users.noreply.github.com>
2025-07-24 07:15:21 -0400

    Add files via upload
    store inserts statements for some of the peachtree tables. also most recent copy of how to get the peachtree id and creating the base registration table.

---
+++ peachtree_inserts.sql
@@ -0,0 +1,10 @@
+--PEACHTREE
+
+insert into peachtree.school_table_reference (normalized_school)
+values ('St. Andre Bessette Schola')
+
+insert into peachtree.school_original (school_original, school_normalized_id)
+values ('St. Andre Bessette Schola (Homeschool Hybrid)', 75)
+
+insert into peachtree.league_hierarchy(organization, sport, league, division, sub_division)
+VALUES ('Peachtree', 'Softball', 'Softball','14u Softball', '12u/14u Softball (Softball age 11 - 12, 13-14)');
